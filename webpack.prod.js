4b1c69ddae9e665a9bfce335db32a5e8c4effb23
Kaj Bialas <kbialas@apptension.com>
2017-02-07 09:07:02 +0100

    Config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,30 @@
 'use strict';
+var webpack = require("webpack");
+var path = require('path');
 
 module.exports = {
-    entry: {},
-    output: {},
-    module: {
-        loaders: []
-    },
+  entry: {
+    'main': './src/main.js'
+  },
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name]-[hash].js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {test: /\.json$/, loader: "json-loader"},
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", 'postcss-loader', "sass-loader"]
+      }
+    ]
+
+  },
     plugins: []
 };
